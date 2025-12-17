65dd6d936464d6a2be0a65120d24e29714097329
ruhanfarajov <ruhanfarajov@gmail.com>
2025-12-17 22:12:01 +0400

    Update 0-release.sh

--- cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-
-s=$(lsb_release -a | grep -w ID)
-
-echo $s | cut -d ':' -f2 | tr -d '\t'| tr -d ' '
+echo $(lsb_release -a | grep -w ID) | cut -d ':' -f2 | tr -d '\t'| tr -d ' '
