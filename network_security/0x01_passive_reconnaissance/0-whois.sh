95c6d320ff74dd0dc260ba44c66d539976c9afaf
ruhanfarajov <ruhanafarajov@gmail.com>
2025-12-28 03:19:47 -0500

    AWK HACKS THE BOX

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech)/{v=$2; if($1~/Street/)v=v" "; if($1~/Ext$/)$1=$1":"; r=r (r==""?"":RS) $1","v} END{printf "%s", r}' > "$1.csv"
+whois "$1" | awk -F: '/^Registrant|^Admin|^Tech/ {if ($1 ~ /Ext/) $1=$1":"; if ($2=="") $2=" "; print $1 "," $2}' > "${1}.csv"
