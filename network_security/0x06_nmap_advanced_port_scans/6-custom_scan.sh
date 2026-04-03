a7d0aef2e29d93b21b93b597d7c8f46d50644233
ruhanfarajov <ruhanfarajov@gmail.com>
2026-04-03 13:59:25 +0400

    nmap tasks

--- network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
+++ network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap -p $2 --scanflags FINSYNPSHRSTACKURG $1 -oN custom_scan.txt 2>/dev/null
+sudo nmap -p "$2" --scanflags FINSYNPSHRSTACKURG "$1" -oN custom_scan.txt > /dev/null 2>&1
