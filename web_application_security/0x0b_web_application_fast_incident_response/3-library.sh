24df4f64baecf6eadb62cb4d2dd44f03fe7caace
ruhanfarajov <ruhanfarajov@gmail.com>
2026-04-12 15:47:23 +0400

    Don't Dare to Attack!

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '{print $12}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}' 
+awk '{print $12}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}' | tr -d '"'
