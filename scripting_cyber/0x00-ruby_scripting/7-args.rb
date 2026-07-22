a597b898db5889bd96d46b7a51ba48112504c8d5
ruhanfarajov <ruhanfarajov@gmail.com>
2026-07-22 16:56:04 +0400

    Refactor print_arguments to simplify output
    Updated the print_arguments function to remove 1-based indexing in output.

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env ruby
 
-# Function that prints command-line arguments with 1-based indexing
+# Function that prints command-line arguments
 def print_arguments
   if ARGV.empty?
     puts "No arguments provided."
   else
+    puts "Arguments:"
     ARGV.each_with_index do |arg, index|
       puts "#{index + 1}. #{arg}"
     end
